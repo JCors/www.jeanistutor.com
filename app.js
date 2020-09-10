1592bce28034dfa4605270cd1d16f63b28db7df0
JCors <joshuacorona33@gmail.com>
2020-09-11 07:19:03 +0800

    Add listener for the port and ejs for footer and header

---
+++ app.js
@@ -0,0 +1,18 @@
+/** @format */
+
+const express = require("express");
+const bodyPaser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyPaser.urlencoded({extended: true}));
+
+app.get("/", () => {
+    console.log("Hello World!");
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
