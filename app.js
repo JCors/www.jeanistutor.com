1103dd59d5646d10a7fe39ada205e237b4dde415
JCors <joshuacorona33@gmail.com>
2020-09-27 12:48:08 +0800

    Data save to the database

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 
 const express = require("express");
 const bodyPaser = require("body-parser");
-const ejs = require("ejs");
-const e = require("express");
 const app = express();
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 //	Server Port
 const PORT = process.env.PORT || 3000;
@@ -16,7 +15,6 @@ const aboutContent =
 	"Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
 	"Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let publishedList = [];
 
 app.use(express.static("public"));
 app.use(bodyPaser.urlencoded({ extended: true }));
@@ -24,11 +22,29 @@ app.use(bodyPaser.urlencoded({ extended: true }));
 // Template Engine
 app.set("view engine", "ejs");
 
+// Connect to the local databaseDatabase
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+// Add Schema for Database
+const postSchema = {
+	title: String,
+	content: String,
+};
+
+//Create Model for the post collection
+const Post = mongoose.model("Post", postSchema);
+
 // Route to Home Page
 app.get("/", function (req, res) {
-	res.render("home", {
-		homeContent: homeStartingContent,
-		posts: publishedList,
+	Post.find({}, function (err, postsContent) {
+		res.render("home", {
+			startingContent: homeStartingContent,
+			posts: postsContent,
+		});
+		console.log("redirected to home");
 	});
 });
 
@@ -42,11 +58,6 @@ app.get("/contact", function (req, res) {
 	res.render("contact", { contactContents: contactContent });
 });
 
-// Route Post About Us
-app.post("/about", function (req, res) {
-	res.redirect("about");
-});
-
 // Route Compose Page
 app.get("/compose", function (req, res) {
 	res.render("compose");
@@ -54,24 +65,30 @@ app.get("/compose", function (req, res) {
 
 // Route to Post Compose Page
 app.post("/compose", function (req, res) {
-	var post = {
-		title: req.body.textTitle,
-		content: req.body.textHere,
-	};
-	publishedList.push(post);
-	res.redirect("/");
+	const post = new Post({
+		title: req.body.postTitle,
+		content: req.body.postBody,
+	});
+	post.save(function (err) {
+		if (!err) {
+			res.redirect("/");
+			console.log("Successfully save in the database");
+		}
+	});
 });
 
 // Post Page
-app.get("/post/:postName", function (req, res) {
-	const requestedTitle = _.lowerCase(req.params.postName);
-	publishedList.forEach(function (post) {
-		const storedTitle = _.lowerCase(post.title);
-		if (storedTitle === requestedTitle) {
-			res.render("post", {
+app.get("/posts/:postId", function (req, res) {
+	const requestedPostId = req.params.postId;
+
+	Post.findOne({ _id: requestedPostId }, function (err, post) {
+		if (!err) {
+			res.render("posts", {
 				title: post.title,
 				content: post.content,
 			});
+		} else {
+			console.log(err);
 		}
 	});
 });
